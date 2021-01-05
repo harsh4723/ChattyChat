9e89260ff5d9000b430114be6be571d95cd6af3a
Harsh Sinha <harsh4723@rediffmail.com>
2021-01-05 23:17:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chattychat/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.chattychat
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(this,drawer_layout,toolbar,R.string.openDrawer,R.string.closeDrawer);
+        drawer_layout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+}
